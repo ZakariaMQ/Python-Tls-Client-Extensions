fca8cf1adf976fb29fed23d562e64e85a10de3b0
zakariaMQ <zm554765@gmail.com>
2024-09-08 13:55:01 +0100

    completed example

--- examples/random_browser_profile.py
+++ examples/random_browser_profile.py
@@ -1,13 +1,19 @@
+from pprint import pprint
 import tls_client
+import sys
+
+sys.path.append("../")
 
 from exts.browser_profiles import BrowserProfileUtil
 
 profile = BrowserProfileUtil() # the profile is already generated
+client_identifier = profile.browser_profile.client_identifier # random identifier has been picked
+headers = profile.browser_profile.headers # the appropriate headers have been picked and the matching user agent as well
 
 session = tls_client.Session(
-    client_identifier=profile.browser_profile.client_identifier,
+    client_identifier=client_identifier,
     random_tls_extension_order=True,
 )
 
-r = session.get("https://httpbin.org/headers", headers=profile.browser_profile.headers)
-print(r.json())
+r = session.get("https://httpbin.org/headers", headers=headers)
+pprint(r.json()["headers"])
